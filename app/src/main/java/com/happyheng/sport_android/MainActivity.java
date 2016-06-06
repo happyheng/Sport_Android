0e91ee6d76a3da268ea8e9573b0d0c8f8b702032
liuheng <dianhuabaobiao@163.com>
2016-06-06 23:14:55 +0800

    1、写好了登陆界面，并添加了一些动画   2、添加了一些第三方的工具类

--- app/src/main/java/com/happyheng/sport_android/MainActivity.java
+++ app/src/main/java/com/happyheng/sport_android/MainActivity.java
@@ -1,13 +1,16 @@
 package com.happyheng.sport_android;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/happyheng/sport_android/activity/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.happyheng.sport_android.activity;
+
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.happyheng.sport_android.R;
+import com.happyheng.sport_android.utils.DensityUtils;
+import com.happyheng.sport_android.utils.ScreenUtils;
+
+/**
+ * "登陆"的Activity
+ * Created by liuheng on 16/6/6.
+ */
+public class LoginActivity extends Activity {
+
+    //动画的时长
+    private static final int ANIMATION_TIME = 1000;
+    //底部Button距中间线的高度
+    private static final int BOTTOM_MAGIN = 50;
+
+
+    private ImageView mIconIv;
+    private EditText mUserNameEt, mPassWordEt;
+    private Button mLoginBt, mRegisterBt;
+
+    private View mTopLl, mBottomLl;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+
+        beginAnimation();
+    }
+
+
+    /**
+     * 初始化所有的View
+     */
+    private void initView() {
+        mIconIv = (ImageView) findViewById(R.id.icon_iv);
+        mUserNameEt = (EditText) findViewById(R.id.username_et);
+        mPassWordEt = (EditText) findViewById(R.id.password_et);
+        mLoginBt = (Button) findViewById(R.id.login_bt);
+        mRegisterBt = (Button) findViewById(R.id.register_bt);
+
+        mTopLl = findViewById(R.id.top_ll);
+        mBottomLl = findViewById(R.id.bottom_ll);
+    }
+
+    /**
+     * 开始动画
+     */
+    private void beginAnimation() {
+
+        final int screenHeight = ScreenUtils.getScreenHeight(this);
+
+        mTopLl.post(new Runnable() {
+            @Override
+            public void run() {
+                int topLlHeight = mTopLl.getMeasuredHeight();
+                int animationHeight = screenHeight / 2 - topLlHeight;
+
+                PropertyValuesHolder holder1 = PropertyValuesHolder.ofFloat("translationY", animationHeight);
+                PropertyValuesHolder holder2 = PropertyValuesHolder.ofFloat("alpha", 0.3f, 1.0f);
+
+                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(mTopLl, holder1, holder2);
+                animator.setDuration(ANIMATION_TIME);
+                animator.start();
+            }
+        });
+
+        mBottomLl.post(new Runnable() {
+            @Override
+            public void run() {
+                int animationHeight = screenHeight / 2 + DensityUtils.dp2px(LoginActivity.this, BOTTOM_MAGIN) - mBottomLl.getTop();
+
+                PropertyValuesHolder holder1 = PropertyValuesHolder.ofFloat("translationY", animationHeight);
+                PropertyValuesHolder holder2 = PropertyValuesHolder.ofFloat("alpha", 0.3f, 1.0f);
+
+                ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(mBottomLl, holder1, holder2);
+                animator.setDuration(ANIMATION_TIME);
+                animator.start();
+            }
+        });
+    }
+}
