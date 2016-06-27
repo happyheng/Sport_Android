a5938961d159992cbd31a7db70cd77adf99e2268
happyheng <happyheng9522@163.com>
2016-06-27 23:00:55 +0800

    给MapActivity加入了定位的功能

--- app/src/main/java/com/happyheng/sport_android/activity/MapActivity.java
+++ app/src/main/java/com/happyheng/sport_android/activity/MapActivity.java
@@ -2,15 +2,26 @@ package com.happyheng.sport_android.activity;
 
 import android.os.Bundle;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
 import com.happyheng.sport_android.R;
+import com.orhanobut.logger.Logger;
 
 /**
  * 地图的Activity
  * Created by liuheng on 16/6/26.
  */
-public class MapActivity extends BaseActivity{
+public class MapActivity extends BaseActivity implements BDLocationListener {
+    //显示的百度MapView
     private MapView mMapView = null;
+
+    //百度地图中定位的类
+    public LocationClient mLocationClient = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,61 @@ public class MapActivity extends BaseActivity{
 
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
+
+        //声明LocationClient类
+        mLocationClient = new LocationClient(getApplicationContext());
+        //给定位类加入自定义的配置
+        initLocation();
+        mLocationClient.registerLocationListener(this);    //注册监听函数
+
+
+        //开始监听
+        mLocationClient.start();
+
+
+    }
+
+    /**
+     * 设置
+     */
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+
+    @Override
+    public void onReceiveLocation(BDLocation bdLocation) {
+        //先暂时获得经纬度信息
+        Logger.d("经度信息为" + bdLocation.getLongitude() + "\n纬度信息为" + bdLocation.getLatitude());
+
+
+        //可以用下面的方法对在得到经纬度信息后在百度地图中进行显示
+        //        BaiduMap mMap = mMapView.getMap();
+//
+//        // 开启定位图层
+//        mMap.setMyLocationEnabled(true);
+//        // 构造定位数据
+//        MyLocationData locData = new MyLocationData.Builder()
+//                .accuracy(location.getRadius())
+//                // 此处设置开发者获取到的方向信息，顺时针0-360
+//                .direction(100).latitude(location.getLatitude())
+//                .longitude(location.getLongitude()).build();
+//        // 设置定位数据
+//        mMap.setMyLocationData(locData);
     }
 
 
@@ -43,5 +109,6 @@ public class MapActivity extends BaseActivity{
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
     }
+
 
 }
