009e2c0fb7fb3474245207253d9ca98bd986de6b
happyheng <happyheng9522@163.com>
2016-07-16 22:18:13 +0800

    做好了sport界面

--- app/src/main/java/com/happyheng/sport_android/view/TabSportBackView.java
+++ app/src/main/java/com/happyheng/sport_android/view/TabSportBackView.java
@@ -2,39 +2,47 @@ package com.happyheng.sport_android.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.happyheng.sport_android.R;
-import com.happyheng.sport_android.utils.DensityUtils;
 
 /**
+ * 运动界面的背景View
  * Created by liuheng on 16/7/16.
  */
 public class TabSportBackView extends View {
 
-    private Paint paint;
-    private int bgHeight;
+    private Paint mPaint;
+    private int mBackHeight,mBackBottomHeight;
+    private Path mBottomPath;
 
     public TabSportBackView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         int backColor = context.getResources().getColor(R.color.sport_bg);
-        paint = new Paint();
-        paint.setColor(backColor);
-        paint.setStyle(Paint.Style.FILL);
+        mPaint = new Paint();
+        mPaint.setColor(backColor);
+        mPaint.setStyle(Paint.Style.FILL);
 
-        bgHeight = DensityUtils.dp2px(context, 305);
+        mBottomPath =new Path();
+
+        mBackHeight = getResources().getDimensionPixelSize(R.dimen.tab_sport_top_height);
+        mBackBottomHeight = getResources().getDimensionPixelSize(R.dimen.tab_sport_oval_half_height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawRect(0, 0, getMeasuredWidth(),bgHeight, paint);
+        canvas.drawRect(0, 0, getMeasuredWidth(), mBackHeight, mPaint);
+
+        mBottomPath.moveTo(0, mBackHeight);
+        mBottomPath.lineTo(getMeasuredWidth()/2, mBackHeight + mBackBottomHeight);
+        mBottomPath.lineTo(getMeasuredWidth(), mBackHeight);
+        mBottomPath.close();
+        canvas.drawPath(mBottomPath,mPaint);
     }
 }
